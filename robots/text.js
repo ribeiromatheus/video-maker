cf4de5d8e92e6c11a25927f83747a7ee216112b0
ribeiromatheus <ribeiro.matheuspereiraribeiro>
2019-03-21 14:49:23 -0300

    feat: add watson natural language understanding service

--- robots/text.js
+++ robots/text.js
@@ -2,10 +2,21 @@ const algorithmia = require('algorithmia')
 const algorithmiaApiKey = require('../credentials/algorithmia.json').apiKey
 const sentenceBoudaryDetection = require('sbd')
 
+const watsonApiKey = require('../credentials/watson-nlu.json').apikey
+const NaturalLanguageUnderstandingV1 = require('watson-developer-cloud/natural-language-understanding/v1.js')
+
+var nlu = new NaturalLanguageUnderstandingV1({
+    iam_apikey: watsonApiKey,
+    version: '2018-04-05',
+    url: 'https://gateway.watsonplatform.net/natural-language-understanding/api/'
+})
+
 async function robot(content) {
     await fetchContentFromWikipedia(content)
     sanitizeContent(content)
     breakContentIntoSentences(content)
+    limitMaximumSentences(content)
+    await fetchKeywordsOfAllSentences(content)
 
     async function fetchContentFromWikipedia(content) {
         const algorithmiaAuthenticated = algorithmia(algorithmiaApiKey)
@@ -18,9 +29,9 @@ async function robot(content) {
     function sanitizeContent(content) {
         const withoutBlankLinesAndMarkdown = removeBlankLinesAndMarkdown(content.sourceContentOriginal)
         const withoutDatesInParentheses = removeDatesInParentheses(withoutBlankLinesAndMarkdown)
-        
+
         content.sourceContentSanitized = withoutDatesInParentheses
-        
+
         function removeBlankLinesAndMarkdown(text) {
             const allLines = text.split('\n')
 
@@ -37,7 +48,7 @@ async function robot(content) {
     }
 
     function removeDatesInParentheses(text) {
-        return text.replace(/\((?:\([^()]*\)|[^()])*\)/gm, '').replace(/  /g,' ')
+        return text.replace(/\((?:\([^()]*\)|[^()])*\)/gm, '').replace(/  /g, ' ')
     }
 
     function breakContentIntoSentences(content) {
@@ -45,13 +56,44 @@ async function robot(content) {
 
         const sentences = sentenceBoudaryDetection.sentences(content.sourceContentSanitized)
         sentences.forEach((sentence) => {
-            content.sentences.push ({
+            content.sentences.push({
                 text: sentence,
                 keywords: [],
                 images: []
             })
         })
-        console.log(sentences)        
+        //console.log(sentences)
+    }
+
+    function limitMaximumSentences(content) {
+        content.sentences = content.sentences.slice(0, content.maximumSentences)
+    }
+    
+    async function fetchKeywordsOfAllSentences(content) {
+        for (const sentence of content.sentences) {
+            sentence.keywords = await fetchWatsonAndReturnKeywords(sentence.text)
+        }
+    }
+
+    async function fetchWatsonAndReturnKeywords(sentence) {
+        return new Promise((resolve, reject) => {
+            nlu.analyze({
+                text: sentence,
+                features: {
+                    keywords: {}
+                }
+            }, (error, response) => {
+                if (error) {
+                    throw error
+                }
+
+                const keywords = response.keywords.map((keyword) => {
+                    return keyword.text
+                })
+
+                resolve(keywords)
+            })
+        })
     }
 }
 
