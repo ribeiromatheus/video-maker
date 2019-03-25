339a723e114e1755ddb52d2e4b24a646a7ac27ca
ribeiromatheus <ribeiro.matheuspereiraribeiro>
2019-03-25 14:42:48 -0300

    feat: add input robot and state robot

--- robots/text.js
+++ robots/text.js
@@ -5,18 +5,24 @@ const sentenceBoudaryDetection = require('sbd')
 const watsonApiKey = require('../credentials/watson-nlu.json').apikey
 const NaturalLanguageUnderstandingV1 = require('watson-developer-cloud/natural-language-understanding/v1.js')
 
-var nlu = new NaturalLanguageUnderstandingV1({
+const nlu = new NaturalLanguageUnderstandingV1({
     iam_apikey: watsonApiKey,
     version: '2018-04-05',
     url: 'https://gateway.watsonplatform.net/natural-language-understanding/api/'
 })
 
-async function robot(content) {
+const state = require('./state.js')
+
+async function robot() {
+    const content = state.load()
+
     await fetchContentFromWikipedia(content)
     sanitizeContent(content)
     breakContentIntoSentences(content)
     limitMaximumSentences(content)
     await fetchKeywordsOfAllSentences(content)
+
+    state.save(content)
 
     async function fetchContentFromWikipedia(content) {
         const algorithmiaAuthenticated = algorithmia(algorithmiaApiKey)
@@ -68,7 +74,7 @@ async function robot(content) {
     function limitMaximumSentences(content) {
         content.sentences = content.sentences.slice(0, content.maximumSentences)
     }
-    
+
     async function fetchKeywordsOfAllSentences(content) {
         for (const sentence of content.sentences) {
             sentence.keywords = await fetchWatsonAndReturnKeywords(sentence.text)
